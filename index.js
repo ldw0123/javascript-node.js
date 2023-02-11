25651049538705f0c701e3774e192ee8df61a4b9
ldw0123 <ldw0123@gmail.com>
2023-02-12 04:22:27 +0900

    230212

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log('Hello World!');
+console.log(1);
+console.log(true);
+console.log({name: '홍길동', age: 20, married: false});
+console.log('Hi!', 100, true, [1, 2, 3]);
+
+console.log('Hello'); // 하이
+/* 사뿐히 즈려밟고 가시옵소서
+console.log('나를 밟고 가라'); */
+console.log('World');
