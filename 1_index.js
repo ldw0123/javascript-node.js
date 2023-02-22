ef4be41e3096c615f2b9d45575e908e7ea10c738
DW <88626857+ldw0123@users.noreply.github.com>
2023-02-23 01:21:52 +0900

    230223

--- 1_index.js
+++ 1_index.js
@@ -7,4 +7,6 @@ console.log('Hi!', 100, true, [1, 2, 3]);
 console.log('Hello'); // 하이
 /* 사뿐히 즈려밟고 가시옵소서
 console.log('나를 밟고 가라'); */
-console.log('World');
+console.log('World!!!');
+console.log('World!!!');
+console.log('World!!!');
