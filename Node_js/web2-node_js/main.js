f7a58b736463d84a3b606b9cc16a1e4d0f42be68
ldw0123 <ldw0123@gmail.com>
2023-11-04 04:04:30 +0900

    study: node.js

--- Node_js/web2-node_js/main.js
+++ Node_js/web2-node_js/main.js
@@ -4,6 +4,34 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url'); // url이라는 모듈을 사용하고, url을 요구한다
 
+function templateHTML(title, list, body) {
+  return ` 
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+  </body>
+  </html>  
+`;
+}
+
+function templateList(filelist) {
+  var list = `<ul>`;
+  var i = 0;
+  while (i < filelist.length) {
+    list = list + `<li><a href='/?id=${filelist[i]}'>${filelist[i]}</a></li>`;
+    i += 1;
+  }
+  list = list + `</ul>`;
+  return list;
+}
+
 var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query; // url을 분석해서 가져옴
@@ -13,60 +41,44 @@ var app = http.createServer(function (request, response) {
   console.log('url : ' + _url);
 
   if (pathname === '/') {
+    // 쿼리 데이터의 id값이 정의되지 않으면 (홈페이지 구현)
     if (queryData.id === undefined) {
-      // 쿼리 데이터의 id값이 정의되지 않으면 (홈페이지 구현)
-      fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+      fs.readdir('./data', function (error, filelist) {
+        console.log(filelist); // filelist 출력
         var title = 'Welcome';
         var description = 'Hello, Node.js';
+        // filelist : data 디렉토리의 파일의 리스트
+        var list = templateList(filelist);
         // 1.html의 내용
-        var template = ` 
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ul>
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </body>
-        </html>  
-    `;
+        var template = templateHTML(
+          title,
+          list,
+          `<h2>${title}</h2> <p>${description}</p>`
+        );
         console.log(template);
         response.writeHead(200);
         response.end(template);
       });
     } else {
-      fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
-        var title = queryData.id;
-        // 1.html의 내용
-        var template = ` 
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ul>
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </body>
-        </html>  
-        `;
-        response.end(template);
-        response.writeHead(200);
+      // id 값이 있으면
+      fs.readdir('./data', function (error, filelist) {
+        console.log(filelist); // filelist 출력
+        fs.readFile(
+          `data/${queryData.id}`,
+          'utf8',
+          function (err, description) {
+            var title = queryData.id;
+            var list = templateList(filelist);
+            // 1.html의 내용
+            var template = templateHTML(
+              title,
+              list,
+              `<h2>${title}</h2> <p>${description}</p>`
+            );
+            response.end(template);
+            response.writeHead(200);
+          }
+        );
       });
     }
   } else {
