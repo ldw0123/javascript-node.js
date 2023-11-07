718e947a3289bc65459d7bbb49c7929b896f60fe
ldw0123 <ldw0123@gmail.com>
2023-11-08 03:47:23 +0900

    study: node.js

--- Node_js/web2-node_js/main.js
+++ Node_js/web2-node_js/main.js
@@ -5,46 +5,43 @@ var fs = require('fs');
 var url = require('url'); // url이라는 모듈을 사용하고, url을 요구한다
 
 function templateHTML(title, list, body) {
-  return ` 
+  return `
   <!doctype html>
   <html>
   <head>
-    <title>WEB - ${title}</title>
+    <title>WEB1 - ${title}</title>
     <meta charset="utf-8">
   </head>
   <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
+    <a href="/create">create</a>
     ${body}
   </body>
-  </html>  
-`;
+  </html>
+  `;
 }
 
 function templateList(filelist) {
-  var list = `<ul>`;
+  var list = '<ul>';
   var i = 0;
   while (i < filelist.length) {
-    list = list + `<li><a href='/?id=${filelist[i]}'>${filelist[i]}</a></li>`;
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
     i += 1;
   }
-  list = list + `</ul>`;
+  list = list + '</ul>';
   return list;
 }
 
 var app = http.createServer(function (request, response) {
   var _url = request.url;
-  var queryData = url.parse(_url, true).query; // url을 분석해서 가져옴
-  var pathname = url.parse(_url, true).pathname;
-
-  console.log('queryData.id : ' + queryData.id); // url의 쿼리 데이터의 id값
-  console.log('url : ' + _url);
-
+  var queryData = url.parse(_url, true).query; // url의 쿼리 스트링을 분석(파싱)해서 가져옴
+  var pathname = url.parse(_url, true).pathname; // url 경로 부분을 파싱해서 가져옴
+  // pathname(url의 경로 부분)이 / 이면
   if (pathname === '/') {
-    // 쿼리 데이터의 id값이 정의되지 않으면 (홈페이지 구현)
+    // 쿼리 스트링의 id값이 정의되지 않으면 (홈페이지 구현)
     if (queryData.id === undefined) {
       fs.readdir('./data', function (error, filelist) {
-        console.log(filelist); // filelist 출력
         var title = 'Welcome';
         var description = 'Hello, Node.js';
         // filelist : data 디렉토리의 파일의 리스트
@@ -53,16 +50,14 @@ var app = http.createServer(function (request, response) {
         var template = templateHTML(
           title,
           list,
-          `<h2>${title}</h2> <p>${description}</p>`
+          `<h2>${title}</h2>${description}`
         );
-        console.log(template);
         response.writeHead(200);
         response.end(template);
       });
     } else {
       // id 값이 있으면
       fs.readdir('./data', function (error, filelist) {
-        console.log(filelist); // filelist 출력
         fs.readFile(
           `data/${queryData.id}`,
           'utf8',
@@ -73,21 +68,43 @@ var app = http.createServer(function (request, response) {
             var template = templateHTML(
               title,
               list,
-              `<h2>${title}</h2> <p>${description}</p>`
+              `<h2>${title}</h2>${description}`
             );
-            response.end(template);
             response.writeHead(200);
+            response.end(template);
           }
         );
       });
     }
+    // pathname이 /create 이면
+  } else if (pathname === '/create') {
+    fs.readdir('./data', function (error, filelist) {
+      var title = 'WEB - create';
+      var list = templateList(filelist);
+      // 1.html의 내용
+      var template = templateHTML(
+        title,
+        list,
+        `
+          <form action="http://localhost:8000/process_create" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p>
+              <textarea name="description" placeholder="description"></textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+        `
+      );
+      response.writeHead(200);
+      response.end(template);
+    });
   } else {
     // 페이지를 찾을 수 없으면
     response.writeHead(404);
     response.end('404 Not found');
   }
 });
-// __dirname + url : 사용자가 접근(요청)할 때 마다 파일을 읽어들인다
-console.log('__dirname + url : ' + __dirname + url);
 
 app.listen(8000);
