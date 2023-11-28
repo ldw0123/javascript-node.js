a2d2ce1c546cccedf5d8bfe031afd487dcc65794
ldw0123 <ldw0123@gmail.com>
2023-11-29 03:41:08 +0900

    study: express

--- express/main.js
+++ express/main.js
@@ -11,14 +11,15 @@ const sanitizeHtml = require('sanitize-html');
 const template = require('./lib/template.js');
 
 // ⭐️ 미들웨어: Express.js 프레임워크에서 HTTP 요청과 응답을 처리하기 위한 중간에 위치한 함수이다
-// 미들웨어는 app.use() 메서드를 통해 Express 애플리케이션에 추가한다.
+// 미들웨어는 주로 app.use() 메서드를 통해 Express 애플리케이션에 추가한다.
 
 // ⭐️ body-parser 미들웨어: 클라이언트에서 서버로 전송된 HTTP 요청의 body(본문. 데이터가 들어가는 부분)를 추출한다
 app.use(bodyParser.urlencoded({ extended: false }));
-// ⭐️ compression 미들웨어: 페이지 압축
+// ⭐️ compression 미들웨어: 페이지 크기 압축
 app.use(compression());
 // 미들웨어 만들기
-app.use(function (req, res, next) {
+app.get('*', function (req, res, next) {
+  // '*' : 모든 요청(req). app.get() 이기 때문에 get 방식으로 들어오는 모든 요청
   fs.readdir('./data', function (error, filelist) {
     req.list = filelist;
     next();
